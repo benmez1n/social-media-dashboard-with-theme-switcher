ebe600e9cd417c8e79680d0e8e7403d1bc1b64ec
benmez1n <ghomri.imad1@gmail.com>
2021-08-10 18:21:50 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let switcher = document.getElementById('switcher');
+switcher.addEventListener('click',function(){
+    if(document.body.classList.contains('dark')){
+        document.body.classList.add('light');
+        document.body.classList.remove('dark');   
+    }
+    else{
+        document.body.classList.add('dark');
+        document.body.classList.remove('light');   
+    }
+},false);
